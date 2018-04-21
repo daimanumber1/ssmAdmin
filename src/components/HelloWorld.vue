116d46e05821379a2d123c1b738a5726cfbf3bea
hujifeiisgood <739736709@qq.com>
2018-04-21 19:22:27 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,23 +2,40 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
+    <button @click=fun()>11111</button>
   </div>
 </template>
-
 <script>
+import axios from "axios";
+axios.defaults.withCredentials = true;
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {
+    fun() {
+      console.log(1);
+      axios
+        .get("http://localhost:8080/ssm/hello")
+        .then(function(response) {
+          console.log(response);
+          console.log(response.data.username);
+          })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
